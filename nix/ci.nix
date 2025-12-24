cb1404868c8c5b3f6054c5dd943524c5172b8967
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-12-24 22:13:28 +0000

    feat: add nix-auto-ci

---
+++ nix/ci.nix
@@ -0,0 +1,54 @@
+{
+  inputs,
+  self,
+  ...
+}:
+{
+  imports = [
+    inputs.git-hooks.flakeModule
+    inputs.actions-nix.flakeModules.default
+    inputs.nix-auto-ci.flakeModule
+  ];
+
+  flake-file.inputs = {
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "";
+      };
+    };
+    actions-nix = {
+      url = "github:nialov/actions.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        git-hooks.follows = "git-hooks";
+      };
+    };
+    nix-auto-ci = {
+      url = "github:aigis-llm/nix-auto-ci";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        git-hooks.follows = "git-hooks";
+        actions-nix.follows = "actions-nix";
+      };
+    };
+  };
+
+  flake.actions-nix = {
+    defaults = {
+      jobs = {
+        timeout-minutes = 60;
+        runs-on = "ubuntu-latest";
+      };
+    };
+    workflows = {
+      ".github/workflows/nix-x86_64-linux.yaml" = inputs.nix-auto-ci.lib.makeNixGithubAction {
+        flake = self;
+        useLix = true;
+      };
+    };
+  };
+}

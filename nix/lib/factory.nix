9adb1fd420b3bdfc051a7d85619421fd20e76428
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-12-24 20:19:02 +0000

    fix: deadnix removed an important thing

--- nix/lib/factory.nix
+++ nix/lib/factory.nix
@@ -4,7 +4,13 @@
 }:
 {
   # Factory function for the library, based on https://github.com/yunfachi/denix/blob/d90f816/lib/configurations/default.nix#L10
-  flake.lib.factory = args: {
-    module = self.lib.internal.module args;
-  };
+  flake.lib.factory =
+    {
+      genericConfigName ? "my",
+    }:
+    {
+      module = self.lib.internal.module {
+        inherit genericConfigName;
+      };
+    };
 }
